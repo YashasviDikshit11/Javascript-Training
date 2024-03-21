1568068d1e2541bf4948ecc9e2bc21c83b111531
YashasviDikshit11 <yashasvi.dikshit@successive.tech>
2024-03-21 11:01:53 +0530

    Updated Assignment 4

---
+++ assignment4/ques2.js
@@ -0,0 +1,11 @@
+// Write a program to reverse a string ("Hello John" => "olleH nhoJ")
+function reverseString(str) {return str.split("").reverse().join("");}
+
+let str = "Hello John";
+let arr = str.split(" ");
+let result = "";
+for (let i of arr) {
+    result += reverseString(i);
+    result += " ";
+}
+console.log(result);
