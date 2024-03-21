d522c13a8d577d3ff9f9e33781c6fa3e85984f4f
YashasviDikshit11 <yashasvi.dikshit@successive.tech>
2024-03-21 10:39:33 +0530

    Corrected Assignment7

--- assignment4/ques4.js
+++
@@ -1,9 +0,0 @@
-// Write a program to convert given string to upperCase OR lowerCase
-function convertToUppercase(str) {
-    return str.toUpperCase();
-}
-function convertToLowercase(str) {
-    return str.toLowerCase();
-}
-console.log(convertToUppercase("hello"));
-console.log(convertToLowercase("HAPPY"));
